80a7582d77b32a9a1523f70f1616504c787cf2c8
4CBCramzz <cramz992@yandex.ru>
2024-02-16 10:34:37 +0300

    Сделали код создающий папки для фоток машин и нашли фотки для двух из 6

--- controllers/create.php
+++ controllers/create.php
@@ -1,25 +1,21 @@
 <?php 
-    require_once('controllers/connect.php');
+    require_once('connect.php');
 
     if ($link->connect_error) {
         die("Ошибка подключения к базе данных: " . $link->connect_error);
     }
 
-    // Получаем список автомобилей из базы данных
-    $sql = "SELECT id, name FROM catalogg";
+    $sql = "SELECT id, fullname FROM catalogg";
     $result = $link->query($sql);
 
     if ($result->num_rows > 0) {
-        // Перебираем результирующий набор
         while ($row = $result->fetch_assoc()) {
             $id = $row["id"];
             $name = $row["fullname"];
             $folderName = $id . "-" . preg_replace("/[^A-Za-z0-9]/", "", $name);
 
-            // Проверяем, существует ли папка с таким названием
-            $folderPath = "templates/img/photots/" . $folderName;
+            $folderPath = "../templates/img/photots/" . $folderName;
             if (!is_dir($folderPath)) {
-                // Создаем новую папку
                 mkdir($folderPath);
                 echo "Папка " . $folderName . " успешно создана.<br>";
             }
